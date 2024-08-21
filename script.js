334c7292837cadd179cb7393ba17a140dcf3863b
Ahmed Alaa Abdelrahman <105639245+AhmedAlaa19@users.noreply.github.com>
2024-08-21 17:33:06 +0300

    adding the project

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleDiv() {
+    var div = document.getElementById("DivCard");
+    if (div.style.display === "none" || div.style.display === "") {
+        div.style.display = "block";
+    } else {
+        div.style.display = "none";
+    }
+}
